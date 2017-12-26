1d4f8dee1fa146c94bb07671f89c02ca219f98b1
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-26 16:54:54 +0100

    hell a lot of changes

--- app/src/main/java/com/example/android/justjava/MainActivity.kt
+++ app/src/main/java/com/example/android/justjava/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.android.justjava
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import android.widget.Toast
+import com.example.android.justjava.data.OrderDetails
 
 import java.text.NumberFormat
-import java.util.*
-import kotlin.collections.ArrayList
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,17 +20,17 @@ class MainActivity : AppCompatActivity() {
         @JvmField val TAG = MainActivity::class.java.simpleName!!
     }
 
-    enum class Toppings { WhippedCream, Chocolate }
-
-    private var mQuantity = 0
-    private var mToppingsList = ArrayList<Toppings>()
-    private var mName : String = ""
+    private lateinit var mOrderDetails : OrderDetails
 
     private lateinit var mEtNameHandler : Handler
+
+    // Lifecycle handlers
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mOrderDetails = OrderDetails(applicationContext)
 
         mEtNameHandler = Handler()
         et_name.addTextChangedListener(object : TextWatcher {
@@ -41,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                b_order.isEnabled = false
                 mEtNameHandler.removeCallbacks(updateName)
                 mEtNameHandler.postDelayed(updateName, 1000)
             }
@@ -51,128 +50,59 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
-        calculateToppings()
+        onWhippedCreamCheckboxClicked(cb_whipped_cream)
+        onChocolateCheckboxClicked(cb_chocolate)
         refreshViews()
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun submitOrder(view: View) {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "*/*"
-        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("justjava@mailinator.com"))
-        // intent.data = Uri.parse("mailto:")
-        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject, mName))
-        intent.putExtra(Intent.EXTRA_TEXT, tv_order_summary.text.toString())
-        Log.d(TAG, "sending e-mail")
-        if (intent.resolveActivity(packageManager) != null) {
-            Log.d(TAG, "really sending e-mail")
-            startActivity(intent)
-        }
-        showToast(getString(R.string.toast_button_clicked))
-    }
+    // Misc functions
 
     fun refreshViews() {
-        display(mQuantity)
-        displayOrderSummary(mName, mQuantity, mToppingsList)
-    }
-
-    private fun calculatePrice(quantity: Int, toppingsList: List<Toppings>) : Int {
-        val unitPrice = 5 + toppingsList.sumBy { when (it) {
-                                                     Toppings.WhippedCream -> 1
-                                                     Toppings.Chocolate -> 2
-                                               } }
-        return unitPrice * quantity
-    }
-
-    private fun display(quantity: Int) {
-        tv_quantity.text = NumberFormat.getInstance().format(quantity.toLong())
-    }
-
-    private fun displayOrderSummary(name : String, quantity: Int, toppingsList: List<Toppings>) {
-        val orderSummary: String = createOrderSummary(name, quantity, toppingsList)
-        tv_order_summary.text = orderSummary
-    }
-
-    private fun createOrderSummary(name : String, quantity: Int, toppingsList: List<Toppings>): String {
-        val orderSummary = StringBuilder("")
-        val price = calculatePrice(quantity, toppingsList)
-        val priceMessage: String
-        when {
-            price == 0 -> priceMessage = getString(R.string.free)
-            price > 0 -> {
-                val format = NumberFormat.getCurrencyInstance()
-                format.currency = Currency.getInstance("USD")
-                priceMessage = getString(R.string.total)+": " + format.format(price.toLong())
-            }
-            else -> priceMessage = "== ERROR ==" // dead code, negative not allowed any more
-        }
-        val quantityString = getText(R.string.quantity)
-        val nameString = getText(R.string.name)
-        val toppingsString = toppingsString(toppingsList)
-        orderSummary.append("$nameString: $name\n")
-                    .append("$quantityString: $quantity\n")
-                    .append(toppingsString)
-                    .append("$priceMessage\n")
-                    .append(getString(R.string.thank_you))
-        return orderSummary.toString()
-    }
-
-    private fun toppingsString(toppingsList: List<Toppings>): String {
-        val toppings = StringBuilder("")
-        toppingsList.map { when (it) {
-                               Toppings.WhippedCream -> getString(R.string.whipped_cream)
-                               Toppings.Chocolate -> getString(R.string.chocolate)
-                         } }.forEach { toppings.append("\t$it\n") }
-        return toppings.toString()
-    }
-
-    private fun calculateToppings() {
-        Log.d(TAG, "calculateToppings: whipped cream: "+cb_whipped_cream.isChecked)
-        mToppingsList = ArrayList()
-        if (cb_whipped_cream.isChecked) {
-            mToppingsList.add(Toppings.WhippedCream)
-        } else {
-            mToppingsList.remove(Toppings.WhippedCream)
-        }
-        Log.d(TAG, "calculateToppings: chocolate: "+cb_chocolate.isChecked)
-        if (cb_chocolate.isChecked) {
-            mToppingsList.add(Toppings.Chocolate)
-        } else {
-            mToppingsList.remove(Toppings.Chocolate)
-        }
-        Log.d(TAG, "toppings: " + mToppingsList)
-    }
-
-    inner class UpdateName : Runnable {
-        override fun run() {
-            mName = et_name.text.toString()
-            refreshViews()
-        }
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    fun refreshToppings(view: View) {
-        calculateToppings()
-        refreshViews()
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    fun decrementQuantity(view: View) {
-        if (mQuantity > 0) {
-            mQuantity--
-            refreshViews()
-        }
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    fun incrementQuantity(view: View) {
-        mQuantity++
-        refreshViews()
+        tv_quantity.text = NumberFormat.getInstance().format(mOrderDetails.getQuantity().toLong())
+        tv_order_summary.text = mOrderDetails.createOrderSummary()
+        b_order.isEnabled = mOrderDetails.isOrderAvailable
     }
 
     private fun showToast(s: String) {
         val toast = Toast.makeText(this, s, Toast.LENGTH_LONG)
         toast.show()
+    }
+
+    // View event handlers
+
+    @Suppress("UNUSED_PARAMETER")
+    fun onOrderButtonClicked(view: View) {
+        mOrderDetails.sendOrder()
+        showToast(getString(R.string.toast_button_clicked))
+    }
+
+    inner class UpdateName : Runnable {
+        override fun run() {
+            mOrderDetails.mName = et_name.text.toString()
+            refreshViews()
+        }
+    }
+
+    fun onWhippedCreamCheckboxClicked(view: View) {
+        if (view is CheckBox) mOrderDetails.setTopping(OrderDetails.Topping.WhippedCream, view.isChecked)
+        refreshViews()
+    }
+
+    fun onChocolateCheckboxClicked(view: View) {
+        if (view is CheckBox) mOrderDetails.setTopping(OrderDetails.Topping.Chocolate, view.isChecked)
+        refreshViews()
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun onQuantityDownButtonClicked(view: View) {
+        mOrderDetails.decrementQuantity()
+        refreshViews()
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun onQuantityUpButtonClicked(view: View) {
+        mOrderDetails.incrementQuantity()
+        refreshViews()
     }
 
 }
---
+++ app/src/main/java/com/example/android/justjava/data/OrderDetails.kt
@@ -0,0 +1,109 @@
+package com.example.android.justjava.data
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.example.android.justjava.R
+import com.example.android.justjava.R.string.toppings
+import java.text.NumberFormat
+import java.util.*
+
+class OrderDetails(private val mContext: Context) {
+
+    companion object {
+        @JvmField val TAG = OrderDetails::class.java.simpleName!!
+    }
+
+    enum class Topping(val textId : Int, val price : Int) {
+        WhippedCream(R.string.whipped_cream, 1),
+        Chocolate(R.string.chocolate, 2)
+    }
+
+    private var mQuantity = 0
+    private val mToppingsList = HashMap<Topping, Boolean>()
+    var mName : String = ""
+
+    var isOrderAvailable : Boolean = false
+        get() = mQuantity > 0
+
+    init {
+        Topping.values().forEach {
+            mToppingsList[it] = false
+        }
+    }
+
+    private fun calculatePrice() : Int {
+        val unitPrice = 5 + mToppingsList.keys.filter { it -> mToppingsList[it] == true }.
+                sumBy { it -> it.price }
+        Log.d(TAG, "unitPrice: " + unitPrice)
+        return unitPrice * mQuantity
+    }
+
+    fun createOrderSummary(): String {
+        val orderSummary = StringBuilder("")
+        val price = calculatePrice()
+        val priceMessage: String
+        when {
+            price == 0 -> priceMessage = mContext.getString(R.string.free)
+            price > 0 -> {
+                val format = NumberFormat.getCurrencyInstance()
+                format.currency = Currency.getInstance("USD")
+                priceMessage = mContext.getString(R.string.total)+": " + format.format(price.toLong())
+            }
+            else -> priceMessage = "== ERROR ==" // dead code, negative not allowed any more
+        }
+        val quantityString = mContext.getString(R.string.quantity)
+        val nameString = mContext.getString(R.string.name)
+        val toppingsString = toppingsString()
+        orderSummary.append("$nameString: $mName\n")
+                .append("$quantityString: $quantityString\n")
+                .append(toppingsString)
+                .append("$priceMessage\n")
+                .append(mContext.getString(R.string.thank_you))
+        return orderSummary.toString()
+    }
+
+    private fun toppingsString(): String {
+        val toppings = StringBuilder("")
+        mToppingsList.keys.
+                filter { it -> mToppingsList[it]!! }.
+                map { it -> mContext.getString(it.textId) }.
+                forEach { toppings.append("\t$it\n") }
+        return toppings.toString()
+    }
+
+    fun sendOrder() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "*/*"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("justjava@mailinator.com"))
+        // intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_SUBJECT, mContext.getString(R.string.subject, mName))
+        intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary())
+        Log.d(TAG, "sending e-mail")
+        if (intent.resolveActivity(mContext.packageManager) != null) {
+            Log.d(TAG, "really sending e-mail")
+            mContext.startActivity(intent)
+        }
+    }
+
+    fun setTopping(topping : Topping, value : Boolean) {
+        Log.d(TAG, "setTopping: " + topping.name + ": "+value)
+        mToppingsList[topping] = value
+        Log.d(TAG, "toppings: " + mToppingsList)
+    }
+
+    fun getQuantity() : Int {
+        return mQuantity
+    }
+
+    fun incrementQuantity() {
+        mQuantity++
+    }
+
+    fun decrementQuantity() {
+        if (mQuantity > 1) {
+            --mQuantity
+        }
+    }
+
+}
